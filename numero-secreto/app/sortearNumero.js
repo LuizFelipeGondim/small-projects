ec31ec59e1e08f07acace51c6c7d3546df44fd0f
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-03-28 23:20:51 -0300

    created the function to choose secret number

---
+++ numero-secreto/app/sortearNumero.js
@@ -0,0 +1,13 @@
+const menorNumero = 1
+const maiorNumero = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorNumero + 1)
+}
+
+const elementoMenorValor = document.getElementById("menor-valor")
+elementoMenorValor.innerHTML = menorNumero
+
+const elementoMaiorValor = document.getElementById("maior-valor")
+elementoMaiorValor.innerHTML = maiorNumero
