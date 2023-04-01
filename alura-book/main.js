f6699e779e889e3a845f2b644cbbbc0826d9a6f6
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-04-01 03:11:07 -0300

    create buscaEndereco function

---
+++ alura-book/main.js
@@ -0,0 +1,27 @@
+async function buscaEndereco(cep) {
+
+    try {
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        const cidade = document.getElementById('cidade');
+        const logradouro = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+
+        console.log(erro);
+    }
+
+}
+
+const cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
