23f97d98607c825058d64fc94f007349a1235a6a
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-04-09 21:18:24 -0300

    created function validates cpf

---
+++ monibank-main/js/script.js
@@ -0,0 +1,18 @@
+import validaCPF from "./validaCPF.js"
+import validaMaioridade from "./validaIdade.js"
+
+const camposDoFormulario = document.querySelectorAll("[required]")
+
+camposDoFormulario.forEach( campo => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+})
+
+function verificaCampo(campo) {
+    if (campo.name === "cpf" && campo.value.length >= 11) {
+        validaCPF(campo)
+    }
+
+    if (campo.name == "aniversario" && campo.value != "") {
+        validaMaioridade(campo)
+    }
+}
