c2b082bc9e0a0bf145be4fd8e5f4d02ef78ca533
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-04-09 22:21:02 -0300

    initial form finalized

--- monibank-main/js/validaCPF.js
+++ monibank-main/js/validaCPF.js
@@ -2,9 +2,7 @@ export default function validaCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "")
 
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse CPF não existe!")
-    } else {
-        console.log("Existe!")
+        campo.setCustomValidity("Esse CPF não existe!")
     }
 }
 
--- monibank-main/js/validaMaioridade.js
+++ monibank-main/js/validaMaioridade.js
@@ -1,6 +1,8 @@
 export default function validaMaioridade(campo) {
     const dataNascimento = new Date(campo.value)
-    validaIdade(dataNascimento)
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity("O usuário não é maior de idade")
+    }
 
 }
 
