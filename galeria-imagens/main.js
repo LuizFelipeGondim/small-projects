49749c851c6d8c155de5189f681ad6342b11817c
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-04-18 19:59:38 -0300

    finished project

--- galeria-imagens/main.js
+++ galeria-imagens/main.js
@@ -4,15 +4,25 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    newImage.setAttribute('alt', `ilustration pic${i}`);
 
-/* Declaring the alternative text for each image file */
+    newImage.addEventListener('click', event => {
+        displayedImage.setAttribute('src', event.target.src)
+    })
 
-/* Looping through images */
+    thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+btn.addEventListener('click', function() {
+    const background = overlay.style.backgroundColor
 
-/* Wiring up the Darken/Lighten button */
+    if (background == "rgba(0, 0, 0, 0)") {
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+    } else {
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0)"
+    }
+    
+})
